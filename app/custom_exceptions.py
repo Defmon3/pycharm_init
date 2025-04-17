efd181b14fad6ab070b894d315897347b68398d8
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2025-04-17 21:27:53 +0200

    Init

---
+++ app/custom_exceptions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# /// script
+# requires-python = "==3.12.9"
+# dependencies = []
+# ///
+
+"""
+SPDX-License-Identifier: LicenseRef-NonCommercial-Only
+© 2025 github.com/defmon3 — Non-commercial use only. Commercial use requires permission.
+Format docstrings according to PEP 287
+File: custom_exceptions.py
+
+"""
+
+
+class ConfigurationError(Exception):
+    """Custom exception for configuration errors."""
