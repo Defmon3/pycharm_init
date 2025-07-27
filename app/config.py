ff90aee65622c6c2408134c794eb161ca9d94937
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2025-07-27 10:36:28 +0200

    Init

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ SPDX-License-Identifier: LicenseRef-NonCommercial-Only
 © 2025 github.com/defmon3 — Non-commercial use only. Commercial use requires permission.
 Format docstrings according to PEP 287
 File: config.py
-
+uv add pydantic pydantic-settings pydantic[email] --no-cache-dir
 """
 
 from pydantic import Field, EmailStr
